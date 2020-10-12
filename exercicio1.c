4b059678d84c97baf3a27ff7a093b5bb0cd40ead
Enzo <enzofg249@gmail.com>
2020-10-12 18:03:32 -0300

    new commit

---
+++ exercicio1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int main(){
+    int i;
+
+    char  nome[50];   
+
+    printf("diga seu nome:\n");
+    scanf("%s", nome);
+
+    i = strlen(nome);
+
+
+
+
+
+}
